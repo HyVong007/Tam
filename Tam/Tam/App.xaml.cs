e419446b03c1ce3489322fa3050efaabcfa64c71
Nguyễn Thanh Tâm <tabandroidjava@gmail.com>
2019-10-04 21:42:56 +0700

    Add project files.

---
+++ Tam/Tam/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SQLite;
+using System.IO;
+using SQLitePCL;
+
+
+namespace Tam
+{
+	public partial class App : Application
+	{
+		public static readonly SQLiteAsyncConnection sqlConnection = new SQLiteAsyncConnection(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "tam.db3"));
+
+
+
+		public App()
+		{
+			InitializeComponent();
+			MainPage = new MainPage();
+		}
+
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+
+
+
+	public class Item
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string s { get; set; }
+
+
+		public override string ToString() => s;
+	}
+
+}
---
+++ Tam/Tam/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Firebase.Storage;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Diagnostics;
+using Firebase.Database.Extensions;
+
+
+namespace Tam
+{
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			// Get any Stream - it can be FileStream, MemoryStream or any other type of Stream
+			var stream = typeof(App).Assembly.GetManifestResourceStream("Tam.images.gai.jpg");
+
+			// Constructr FirebaseStorage, path to where you want to upload the file and Put it there
+			var task = new FirebaseStorage("learn-firebase-tam.appspot.com")
+				.Child("tam").Child("gai dep.jpg")
+				.PutAsync(stream);
+
+			// Track progress of the upload
+			task.Progress.ProgressChanged += (s, ee) => Debug.WriteLine($"Progress: {ee.Percentage} %");
+
+			// await the task to wait until upload completes and get the download url
+			string downloadUrl = await task;
+		}
+	}
+}
